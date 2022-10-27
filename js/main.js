bec551e1d27d66e600a3b2a9678eb9328840d45f
ippoRulez <ippolitodonato@gmail.com>
2022-10-27 17:53:32 +0200

    added some more logic and conditionals

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-console.log('Ciao');
+const firstPrice = document.getElementById('firstPrice');
+const actualPrice = document.getElementById('actualPrice');
 
-const kmNeeded = prompt('Quanti km devi fare?');
-const age = prompt('Inserisci la tua eta: ');
+const kmNeeded = parseFloat(prompt('Quanti km devi fare?'));
+const age = parseInt(prompt('Inserisci la tua eta: '));
+
 const kmPrice = 0.21;
 const discount20 = 0.8;
-const doscount40 = 0.6;
-let ticketPrice;
+const discount40 = 0.6;
+
+const ticketPrice = kmPrice * kmNeeded;
+let discountedPrice = ticketPrice;
 
 if (age < 18) {
-	ticketPrice = kmPrice * kmNeeded * discount20;
-	console.log(ticketPrice);
-} else {
-	console.log('impossibile calcolare');
+	discountedPrice = ticketPrice * discount20;
+} else if (age > 65) {
+	discountedPrice = ticketPrice * discount40;
 }
+
+firstPrice.innerHTML = ticketPrice.toFixed(2);
+actualPrice.innerHTML = discountedPrice.toFixed(2);
